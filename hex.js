1909865fff0e4196640b164a16f9a1ce51980fed
Ben Hayden <ben.hayden09@gmail.comm>
2023-02-25 11:20:14 +0000

    init

---
+++ hex.js
@@ -0,0 +1,19 @@
+
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+// numbers represent units & letter represent to multiple of 10: A = 10, B = 20 and so on
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
